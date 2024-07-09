2389a6fedd500dcb13ccb12ab478890af4be4273
Megha PANCHAL <m.panchal@technologyandstrategy.com>
2024-07-09 17:48:15 +0200

    Added initial authentication app

--- litreview/authentication/apps.py
+++ litreview/authentication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AskingReviewsConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'asking_reviews'
+    name = 'authentication'
