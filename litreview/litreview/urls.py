170f1892a8681203104c326e58f42cfbcd3258a1
Megha PANCHAL <m.panchal@technologyandstrategy.com>
2024-07-10 20:58:12 +0200

    Updated login feature to redirect authenticated user

--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
 import authentication.views
 import feed.views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+        name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', feed.views.feed_home_page, name='home'),
 ]
