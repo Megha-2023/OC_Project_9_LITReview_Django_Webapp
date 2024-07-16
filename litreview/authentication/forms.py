a05c7ec2c2fb96d898f1de0cd275f2839e7eb234
Megha PANCHAL <m.panchal@technologyandstrategy.com>
2024-07-16 20:32:27 +0200

    Added models in feed as per DB Schema

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -1,5 +1,8 @@
-from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=60, widget=forms.PasswordInput)
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+
--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView
 import authentication.views
 import feed.views
 
+auth_patterns = [path('logout/', authentication.views.logout_user, name='logout'),
+                 path('signup/', authentication.views.signup_page, name='signup'),
+                 path('login/', LoginView.as_view(
+                    template_name='authentication/login.html',
+                    redirect_authenticated_user=True),
+                    name='login')]
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', LoginView.as_view(
-        template_name='authentication/login.html',
-        redirect_authenticated_user=True),
-        name='login'),
-    path('logout/', authentication.views.logout_user, name='logout'),
-    path('home/', feed.views.feed_home_page, name='home'),
+    path('', feed.views.feed_home_page, name='home'),
+    path('auth/', include(auth_patterns))
 ]
